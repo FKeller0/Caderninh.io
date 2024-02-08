19b1555aa0cf3af1b1ceecb56cf67b0c4dc959f0
Fernando Raphael Keller <fkeller_3@hotmail.com>
2024-02-08 16:37:20 -0300

    Added domain models for Notes and for Users

---
+++ Caderninh.io.Domain/Notes/NoteCategory.cs
@@ -0,0 +1,29 @@
+﻿using ErrorOr;
+using Throw;
+
+namespace Caderninh.io.Domain.Notes
+{
+    public class NoteCategory
+    {
+        public Guid Id { get; private set; }
+        public Guid UserId { get; private set; }
+        public List<Guid> _noteIds { get; set; } = new();
+
+        public string Name { get; set; } = null!;
+
+        public NoteCategory(string name, Guid userId)
+        {
+            Name = name;
+            UserId = userId;
+        }
+
+        public ErrorOr<Success> AddNotes(Notes notes) 
+        {
+            _noteIds.Throw().IfContains(notes.Id);
+
+            _noteIds.Add(notes.Id);
+
+            return Result.Success;
+        }
+    }
+}
---
+++ Caderninh.io.Domain/Notes/Notes.cs
@@ -0,0 +1,23 @@
+﻿namespace Caderninh.io.Domain.Notes
+{
+    public class Notes
+    {
+        public Guid Id { get; }
+
+        public Guid NoteCategoryId { get; }
+
+        public string Body { get; set; } = null!;
+
+        public DateTime CreatedAt { get; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        public Notes(Guid noteCategoryId, string body, Guid? id)
+        {
+            Body = body;
+            NoteCategoryId = noteCategoryId;
+            Id = id ?? Guid.NewGuid();
+            CreatedAt = DateTime.Now;
+        }
+    }    
+}
