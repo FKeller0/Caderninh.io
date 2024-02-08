429280e6760b1f9b0c3b81e2954c63c9c6e0f4e1
Fernando Raphael Keller <fkeller_3@hotmail.com>
2024-02-08 16:56:09 -0300

    Changes to UserModel for future Authentication

--- Caderninh.io.Domain/User/User.cs
+++ Caderninh.io.Domain/User/User.cs
@@ -1,41 +1,11 @@
-﻿using Caderninh.io.Domain.Notes;
-using ErrorOr;
-using Throw;
-
-namespace Caderninh.io.Domain.User
+﻿namespace Caderninh.io.Domain.User
 {
     public class User
     {
-        public Guid Id { get; private set; }
-
-        public List<Guid> _noteCategoryIds = new();
-
+        public Guid Id { get; set; } = Guid.NewGuid();
         public string FirstName { get; set; } = null!;
-
         public string LastName { get; set; } = null!;
-
-        public string Email { get; private set; } = null!;
-
-        public string Password { get; private set; } = null!;
-
-        public User() { }
-
-        public User(string firstName, string lastName, string email, string password, Guid? id = null)
-        {
-            FirstName = firstName;
-            LastName = lastName;
-            Email = email;
-            Password = password;
-            Id = id ?? Guid.NewGuid();
-        }
-
-        public ErrorOr<Success> AddNoteCategory(NoteCategory noteCategory)
-        {
-            _noteCategoryIds.Throw().IfContains(noteCategory.Id);
-
-            _noteCategoryIds.Add(noteCategory.Id);
-
-            return Result.Success;
-        }
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
     }
 }
