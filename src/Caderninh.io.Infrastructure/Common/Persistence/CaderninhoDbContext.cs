e60570eb6df9e48be076f042989f779323e1dd0a
Fernando Raphael Keller <fkeller_3@hotmail.com>
2024-04-07 15:32:20 -0300

    Added support and updated packages for Sqlite database integration

---
+++ src/Caderninh.io.Infrastructure/Common/Persistence/CaderninhoDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Caderninh.io.Domain.User;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Caderninh.io.Infrastructure.Common.Persistence
+{
+    public class CaderninhoDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; } = null!;
+
+        public CaderninhoDbContext(DbContextOptions options) : base(options) { }
+
+        public async Task CommitChangesAsync() 
+        {
+            await SaveChangesAsync();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
