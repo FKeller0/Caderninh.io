64662a641eed0fcceab38d8e004fc5f107ffad41
Fernando Raphael Keller <fkeller_3@hotmail.com>
2024-04-09 16:03:52 -0300

    Added login support

--- src/Caderninh.io.Application/Authentication/Queries/Login/LoginQueryHandler.cs
+++ src/Caderninh.io.Application/Authentication/Queries/Login/LoginQueryHandler.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MediatR;
+using Caderninh.io.Application.Authentication.Common;
+using ErrorOr;
+using Caderninh.io.Application.Common.Interfaces;
+using Caderninh.io.Domain.Common.Interfaces;
 
 namespace Caderninh.io.Application.Authentication.Queries.Login
 {
-    public class LoginQueryHandler
+    public class LoginQueryHandler(
+        IJwtTokenGenerator _jwtTokenGenerator,
+        IPasswordHasher _passwordHasher,
+        IUsersRepository _usersRepository) : IRequestHandler<LoginQuery, ErrorOr<AuthenticationResult>>
     {
+        public async Task<ErrorOr<AuthenticationResult>> Handle(LoginQuery request, CancellationToken cancellationToken)
+        {
+            var user = await _usersRepository.GetByEmailAsync(request.Email);
+
+            return user is null || !user.IsCorrectPasswordHash(request.Password, _passwordHasher)
+                ? AuthenticationErrors.InvalidCredentials
+                : new AuthenticationResult(user, _jwtTokenGenerator.GenerateToken(user));
+        }
     }
 }
